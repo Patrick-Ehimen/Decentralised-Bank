bc39dd48e5d9ef5520694e18aa19046bb4d12c38
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-01-20 23:04:16 +0100

    Update Bank.sol
    1.Commented the setPassword function. instead i included it in create account.
    
    2. Added a constructor with value of type string set to "De_Bank DAO"

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -4,24 +4,38 @@ pragma solidity ^0.8.0;
 
 contract Bank {
     struct CustomerDetails {
-        string User;
+        string UserName;
         address UserAddr;
         //string AccountPassword;
         bytes32 myPassowrd;
         uint Balance;
         string AccountType;
     }
+    string bank;
 
-    CustomerDetails public customerDetails;
+    //CustomerDetails public customerDetails;
+    CustomerDetails[] public customers;
 
-    //CustomerDetails[] public customerDetails;
+    constructor() {
+        bank = "De_Bank DAO";
+    }
 
-    function setPassword(string memory _accountPassword) public {
-        //customerDetails.AccountPassword = _accountPassword;
-        customerDetails.myPassowrd = keccak256(
+    // function setPassword(string memory _accountPassword) public {
+    //     //customerDetails.AccountPassword = _accountPassword;
+    //     customerDetails.myPassowrd = keccak256(
+    //         abi.encodePacked(_accountPassword)
+    //     );
+    // }
+
+    function createAccount(
+        string memory name,
+        string memory _accountPassword
+    ) public {
+        CustomerDetails storage accountCreated = customers.push();
+        accountCreated.UserName = name;
+        accountCreated.UserAddr = msg.sender;
+        accountCreated.myPassowrd = keccak256(
             abi.encodePacked(_accountPassword)
         );
     }
-
-    function createAccount() public {}
 }
