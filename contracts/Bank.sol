135caf77d08a8e2e2c711475f35cf0c3a2a9c57f
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-01-21 12:25:46 +0100

    Update Bank.sol
    checks if the sender of a message (msg.sender) already has an account (customerExist[msg.sender]) and throws an error if they do ("You already have an account"). If the sender does not have an account, it sets the accountCreated.UserAddr to the msg.sender and sets customerExist[msg.sender] to true, indicating that the sender now has an account.

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -12,9 +12,14 @@ contract Bank {
         string AccountType;
     }
     string bank;
+    string public constant ACCT_TYPE_1 = "Savings";
+    string public constant ACCT_TYPE_2 = "Current";
+    string public constant ACCT_TYPE_3 = "Fixed_Depost";
 
     //CustomerDetails public customerDetails;
     CustomerDetails[] public customers;
+
+    mapping(address => bool) public customerExist;
 
     constructor() {
         bank = "De_Bank DAO";
@@ -33,9 +38,12 @@ contract Bank {
     ) public {
         CustomerDetails storage accountCreated = customers.push();
         accountCreated.UserName = name;
+        require(!customerExist[msg.sender], "You already have an acoount");
         accountCreated.UserAddr = msg.sender;
+        customerExist[msg.sender] = true;
         accountCreated.myPassowrd = keccak256(
             abi.encodePacked(_accountPassword)
         );
+        //return "You already have an acoount";
     }
 }
