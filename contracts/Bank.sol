8bbb0a190f56b251a2dbcb6ef8b2cb61d15851ca
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-01-28 23:21:13 +0100

    Update Bank.sol

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -6,8 +6,22 @@ contract Bank {
     struct CustomerDetails {
         address UserAddr;
         uint AcctBalance;
-        mapping(address => bytes32) passwords;
     }
 
+    mapping(address => bytes32) passwords;
+
+    CustomerDetails[] public customers;
+
     mapping(address => bool) public customerExist;
+
+    function createAccount(string memory password) public {
+        CustomerDetails storage accountCreated = customers.push();
+        require(msg.sender != address(0), "You cannot use a zero address");
+        require(!customerExist[msg.sender], "You already have an acoount");
+        accountCreated.UserAddr = msg.sender;
+        customerExist[msg.sender] = true;
+
+        bytes32 hashedPassword = keccak256(abi.encodePacked(password));
+        passwords[msg.sender] = hashedPassword;
+    }
 }
