36ad2d9690478e1005c2c34cce7131d7c684a544
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-02-02 13:12:11 +0100

    Update hardhat.config.js
    adding network to be deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const MATIC_RPC_URL = process.env.MATIC_RPC_URL;
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    mumbai: {
+      url: MATIC_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
+    },
+  },
   solidity: "0.8.9",
 };
